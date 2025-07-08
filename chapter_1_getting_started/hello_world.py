920e897318f8f3dcfbbee500d52a2999a8b0a967
KessAhmed786786 <kessahmed786@gmail.com>
2025-07-08 20:53:27 +0100

    feat:  sep and end Parameters included

--- chapter_1_getting_started/hello_world.py
+++ chapter_1_getting_started/hello_world.py
@@ -1,1 +1,13 @@
-print("Hello Python world!")
+# Chapter 1 - Getting Started
+# hello_world.py 
+# Various of examples of print, using some parameters
+# Kess Ahmed, 8th July 2025
+
+# Print function
+print("Hello World")
+
+# sep Parameter
+print("Name", "John", sep="-")
+
+# end Parameter
+print("Loading", end="...")
