f71c91d06e7d0903b29a524912eeef117cf1dc24
KessAhmed786786 <kessahmed786@gmail.com>
2025-07-09 16:41:13 +0100

    fix: Added back example code

--- chapter_1_getting_started/ex1-1_pythonorg.py
+++ chapter_1_getting_started/ex1-1_pythonorg.py
@@ -2,3 +2,12 @@
 # ex1-1_pythonorg.py
 # Explore the Python home page (https://python.org) to find topics that interest you. As you become familiar with Python, different parts of the site will be more useful to you.
 # Kess Ahmed, 9th July 2025
+
+# Print function
+print("Hello World")
+
+# sep Parameter
+print("Name", "John", sep="-")
+
+# end Parameter
+print("Loading", end="...")
