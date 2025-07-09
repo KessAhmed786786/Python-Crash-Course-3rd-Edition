862e9a746d306013336c0a46ffe2cbd3387c2e9d
KessAhmed786786 <kessahmed786@gmail.com>
2025-07-09 16:38:17 +0100

    fix: Fixed the eror

--- chapter_1_getting_started/ex1-2_hello_world_typos.py
+++ chapter_1_getting_started/ex1-2_hello_world_typos.py
@@ -3,7 +3,7 @@
 # Open the hello_world.py file you just created. Make a typo somewhere in the line and run the program again. Can you make a typo that generates an error? Can you make sense of the error message? Can you make a typo that doesn’t generate an error? Why do you think it didn’t make an error?
 # Kess Ahmed, 9th July 2025
 
-print("Hello World)
+print("Hello World")
 # SyntaxError: unterminated string literal (detected at line 6)
 # Missing " closing the string literal
 
