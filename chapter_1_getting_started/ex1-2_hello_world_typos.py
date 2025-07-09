10d1dfff42d4e9d6b4d6e9493d46beba13a89483
KessAhmed786786 <kessahmed786@gmail.com>
2025-07-09 16:20:05 +0100

    feat: Exercise 1-2 complete

---
+++ chapter_1_getting_started/ex1-2_hello_world_typos.py
@@ -0,0 +1,12 @@
+# Chapter 1 - Getting Started
+# hello_world.py 
+# Open the hello_world.py file you just created. Make a typo somewhere in the line and run the program again. Can you make a typo that generates an error? Can you make sense of the error message? Can you make a typo that doesn’t generate an error? Why do you think it didn’t make an error?
+# Kess Ahmed, 8th July 2025
+
+print("Hello World)
+# SyntaxError: unterminated string literal (detected at line 6)
+# Missing " closing the string literal
+
+print("Hello Worl")
+# Works
+# Missing letters are not spelled checked, syntax is correct for python because enclosed in double ""
