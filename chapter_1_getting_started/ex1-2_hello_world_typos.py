06ab7b67d1d79b413df444bc86e9d8abc7bd353f
KessAhmed786786 <kessahmed786@gmail.com>
2025-07-09 16:21:38 +0100

    docs: Fixed date

--- chapter_1_getting_started/ex1-2_hello_world_typos.py
+++ chapter_1_getting_started/ex1-2_hello_world_typos.py
@@ -1,7 +1,7 @@
 # Chapter 1 - Getting Started
 # ex1-2_hello_world_typos.py
 # Open the hello_world.py file you just created. Make a typo somewhere in the line and run the program again. Can you make a typo that generates an error? Can you make sense of the error message? Can you make a typo that doesn’t generate an error? Why do you think it didn’t make an error?
-# Kess Ahmed, 8th July 2025
+# Kess Ahmed, 9th July 2025
 
 print("Hello World)
 # SyntaxError: unterminated string literal (detected at line 6)
