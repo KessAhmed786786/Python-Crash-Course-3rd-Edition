56c0300d8c4670d8edb1d8777af81ed8f45218fc
KessAhmed786786 <kessahmed786@gmail.com>
2025-07-09 16:19:39 +0100

    feat: Excercise1-1 complete

---
+++ chapter_1_getting_started/ex1-1_pythonorg.py
@@ -0,0 +1,4 @@
+# Chapter 1 - Getting Started
+# ex1-1_pythonorg.py
+# Explore the Python home page (https://python.org) to find topics that interest you. As you become familiar with Python, different parts of the site will be more useful to you.
+# Kess Ahmed, 9th July 2025
