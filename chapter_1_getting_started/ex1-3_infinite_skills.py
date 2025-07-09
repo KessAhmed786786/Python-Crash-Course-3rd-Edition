92599c2ec9383ddd569d24741f7ae3e7d0c1a3c4
KessAhmed786786 <kessahmed786@gmail.com>
2025-07-09 16:38:34 +0100

    feat: Exercise 1-3 complete

---
+++ chapter_1_getting_started/ex1-3_infinite_skills.py
@@ -0,0 +1,30 @@
+# Chapter 1 - Getting Started
+# ex1-3_infinite_skills.py
+# If you had infinite programming skills, what would you build? You’re about to learn how to program. If you have an end goal in mind, you’ll have an immediate use for your new skills; now is a great time to write brief descriptions of what you want to create. It’s a good habit to keep an “ideas” notebook that you can refer to whenever you want to start a new project. Take a few minutes now to describe three programs you want to create.
+# Kess Ahmed, 9th July 2025
+
+# To begin with:
+# Mad Libs Generator
+# Magic 8-Ball Simulator
+# Dice Rolling Simulator
+
+# Number Guessing Game
+# Basic Calculator
+# To-Do List
+# Unit Converter
+# Password Generator
+# Rock Paper Scissors Game
+# Hangman Game
+# Simple Text Adventure Game
+# Basic Quiz Application
+# Countdown Timer
+# Mad Libs Generator
+# Dice Rolling Simulator
+# Palindrome Checker
+# Simple Contact Manager
+# Basic  Expense Tracker
+# Word Counter
+# Basic Journal/Diary 
+# Magic 8-Ball Simulator
+# Even/Odd Number Sorter
+# Sentence Reverser
