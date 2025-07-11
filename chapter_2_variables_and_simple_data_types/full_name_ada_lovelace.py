90f7438fbf7acbabc9fea39d8365afa66387e78f
KessAhmed786786 <kessahmed786@gmail.com>
2025-07-11 19:29:27 +0100

    feat: Testing f-strings

---
+++ chapter_2_variables_and_simple_data_types/full_name_ada_lovelace.py
@@ -0,0 +1,16 @@
+# Chapter 2 - Variables and Simple Data Types
+# full_name_ada_lovelace.py
+# Combining both the first and last names of a full name and combining together
+# Kess Ahmed, 11th July 2025
+
+# Declare first_name
+first_name = "ada"
+
+# Declare last_name
+last_name = "lovelace"
+
+# Combine into full_name using f-string
+full_name = f"{first_name} {last_name}"
+
+# Print full_name
+print(full_name)
