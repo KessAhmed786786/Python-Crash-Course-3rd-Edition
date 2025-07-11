015aea3f3c54447a10693ae87e88abd5a682664c
KessAhmed786786 <kessahmed786@gmail.com>
2025-07-11 19:00:13 +0100

    feat: Testing .title() function

---
+++ chapter_2_variables_and_simple_data_types/name_ada_lovelace.py
@@ -0,0 +1,10 @@
+# Chapter 2 - Variables and Simple Data Types
+# name_ada_lovelace.py
+# Outputting a capitalized first letter of each word in a string
+# Kess Ahmed, 11th July 2025
+
+# Declaring variable name, no capitals
+name = "ada lovelace"
+
+# Using .title() string function, print tweaked name
+print(name.title())
