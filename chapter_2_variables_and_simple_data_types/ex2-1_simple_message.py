fdeb09d7cd7b42d4e720e881f4df10d41bda5fe8
KessAhmed786786 <kessahmed786@gmail.com>
2025-07-11 18:46:50 +0100

    feat: Exercise 2-1 complete

---
+++ chapter_2_variables_and_simple_data_types/ex2-1_simple_message.py
@@ -0,0 +1,10 @@
+ # Chapter 2 - Variables and Simple Data Types
+# ex2-1_simple_message.py
+# Assign a message to a variable, and then print that message
+# Kess Ahmed, 11th July 2025
+
+# Assigning message
+message = "The message is a strong and clear one"
+
+# Printing message
+print(message)
