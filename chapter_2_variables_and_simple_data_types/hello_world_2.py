c25076bd9a87d581e6de86313f3b10def5889ef2
KessAhmed786786 <kessahmed786@gmail.com>
2025-07-11 18:00:02 +0100

    feat: Testing code from book

---
+++ chapter_2_variables_and_simple_data_types/hello_world_2.py
@@ -0,0 +1,10 @@
+# Chapter 2 - Variables and Simple Data Types
+# hello_world_2.py
+# Reusing the old program, using a variable this time
+# Kess Ahmed, 11th July 2025
+
+# Declaring variable message
+message = "Hello Python world!"
+
+# Print the variable message
+print(message)
