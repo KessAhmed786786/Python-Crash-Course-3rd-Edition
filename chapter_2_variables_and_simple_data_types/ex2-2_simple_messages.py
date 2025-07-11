770752b219bcc9520d096a2edbb790311df7b26c
KessAhmed786786 <kessahmed786@gmail.com>
2025-07-11 18:47:04 +0100

    feat: Exercise 2-2 complete

---
+++ chapter_2_variables_and_simple_data_types/ex2-2_simple_messages.py
@@ -0,0 +1,16 @@
+ # Chapter 2 - Variables and Simple Data Types
+# ex2-1_simple_messages.py
+# Assign a message to a variable, and print that message.Then change the value of the variable to a new message, and print the new message.
+# Kess Ahmed, 11th July 2025
+
+# Assigning message
+message = "The message is a strong and clear one"
+
+# Printing message
+print(message)
+
+# Reassiging a new message
+message = "The message has become old and forgotten"
+
+# Prining the new message
+print(message)
