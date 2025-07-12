c6961b1b9f4adf08e051722ce960769a2b56cd72
KessAhmed786786 <kessahmed786@gmail.com>
2025-07-12 20:39:21 +0100

    feat: Testing .removeprefix funcion

---
+++ chapter_2_variables_and_simple_data_types/remove_prefix.py
@@ -0,0 +1,13 @@
+# Chapter 2 - Variables and Simple Data Types
+# remove_prefix.py
+# Remove https in the string for ease of readability
+# Kess Ahmed, 12th July 2025
+
+# Declare variable
+nostarch_url = "https://nostarch.com"
+
+# Remove the https:// prefix, and reassign
+nostarch_url = nostarch_url.removeprefix("https://")
+
+# Check print
+print(nostarch_url)
