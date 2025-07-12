7ab0ccf71d59d3038d32380989753332ed913858
KessAhmed786786 <kessahmed786@gmail.com>
2025-07-12 20:19:55 +0100

    feat: Testing tabs and newlines in strings

---
+++ chapter_2_variables_and_simple_data_types/tabs_newlines.py
@@ -0,0 +1,16 @@
+# Chapter 2 - Variables and Simple Data Types
+# tabs_newlines.py
+# Output a tab and then a newline, then a list of tabbed newlines
+# Kess Ahmed, 12th July 2025
+
+# Create a tab
+print("\tPython")
+
+# Create a newline
+print("\nPython")
+
+# Create a list of newlines
+print("\nPython\nJavaScript\nC++")
+
+# Create a list of newlines + tabs
+print("\n\tPython\n\tJavaScript\n\tC++")
