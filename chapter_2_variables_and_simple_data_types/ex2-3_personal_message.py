7a2fa98648654cbb44e7873630c9275624506bc6
KessAhmed786786 <kessahmed786@gmail.com>
2025-07-15 18:48:42 +0100

    feat: Exercise 2-3 Complete

---
+++ chapter_2_variables_and_simple_data_types/ex2-3_personal_message.py
@@ -0,0 +1,10 @@
+# Chapter 2 - Variables and Simple Data Types
+# ex2-3_personal_message.py
+# Use a variable to represent a person’s name, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+# Kess Ahmed, 15th July 2025
+
+# Declare person's name variable
+person_name = "Eric"
+
+# Print using f-string
+print(f"Hello {person_name}, would you like to learn some Python today?")
