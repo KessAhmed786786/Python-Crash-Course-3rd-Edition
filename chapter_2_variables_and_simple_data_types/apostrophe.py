04cb89636511710c011408c928e49460bf82d1da
KessAhmed786786 <kessahmed786@gmail.com>
2025-07-15 18:40:35 +0100

    feat: Testing apostrophe example

---
+++ chapter_2_variables_and_simple_data_types/apostrophe.py
@@ -0,0 +1,10 @@
+# Chapter 2 - Variables and Simple Data Types
+# apostrophe.py
+# Correctly outputting a apostrophe within double quotes
+# Kess Ahmed, 15th July 2025
+
+# Declare variable
+message = "One of Python's strengths is its diverse community"
+
+# Print variable
+print(message)
