c7bfaefac190eb90a9c88e9083baf5d14cac4cbd
KessAhmed786786 <kessahmed786@gmail.com>
2025-07-15 18:48:52 +0100

    feat: Exercise 2-4 Complete

---
+++ chapter_2_variables_and_simple_data_types/ex2-4_name_cases.py.py
@@ -0,0 +1,16 @@
+# Chapter 2 - Variables and Simple Data Types
+# ex2-4_name_cases.py
+# Use a variable to represent a person’s name, and then print that person’s name in lowercase, uppercase, and title case.
+# Kess Ahmed, 15th July 2025
+
+# Declare person name
+person_name = "ELsA"
+
+# Print upper
+print(f"{person_name.upper()}")
+
+# Print lower
+print(f"{person_name.lower()}")
+
+# Print title
+print(f"{person_name.title()}")
