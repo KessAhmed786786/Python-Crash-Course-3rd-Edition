14187f6a9beef06c7c2eac75bf5d446f78f1357e
KessAhmed786786 <kessahmed786@gmail.com>
2025-07-16 19:45:49 +0100

    feat: Exercise 2-5 Complete

---
+++ chapter_2_variables_and_simple_data_types/ex2-5_famous_quote.py
@@ -0,0 +1,8 @@
+# Chapter 2 - Variables and Simple Data Types
+# ex2-5_famous_quote.py
+# Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+# Kess Ahmed, 16th July 2025
+
+# Print statement of single and double quotes
+print('Albert Einstein once said "A person who never made a mistake never tried anything new"')
+
