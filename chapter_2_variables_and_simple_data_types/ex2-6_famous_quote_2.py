28eb50231453aa65d0e29ce09358c79b6ed700dd
KessAhmed786786 <kessahmed786@gmail.com>
2025-07-16 19:46:06 +0100

    feat: Exercise 2-6 Complete

---
+++ chapter_2_variables_and_simple_data_types/ex2-6_famous_quote_2.py
@@ -0,0 +1,13 @@
+# Chapter 2 - Variables and Simple Data Types
+# ex2-5_famous_quote_2.py
+# Repeat Exercise 2-5, but this time, represent the famous person’s name using a variable called famous_person. Then compose your message and represent it with a new variable called message. Print your message.
+# Kess Ahmed, 16th July 2025
+
+# Declare famous_person variable
+famous_person = "Albert Einstein"
+
+# Declare message variable, using single double quotes properly
+message = f'{famous_person} once said, "A person who never made a mistake never tried anything new."'
+
+# Print message
+print(message)
