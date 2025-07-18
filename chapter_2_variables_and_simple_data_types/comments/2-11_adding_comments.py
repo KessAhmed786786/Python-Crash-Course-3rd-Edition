2f8b883658d23b3f4e5e1f3d1fbdb63098c008e3
KessAhmed786786 <kessahmed786@gmail.com>
2025-07-18 16:07:40 +0100

    feat: Comment section complete

---
+++ chapter_2_variables_and_simple_data_types/comments/2-11_adding_comments.py
@@ -0,0 +1,7 @@
+# Chapter 2 - Variables and Simple Data Types
+# ex2-11_adding_comments.py
+# Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+# Kess Ahmed, 18th July 2025
+
+# Expain the point of the program
+print("Showing off the comments in the .py file")
