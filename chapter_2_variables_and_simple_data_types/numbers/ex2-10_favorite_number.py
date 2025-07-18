e6cfe9d024644c8994ab9073126eeb676198fec7
KessAhmed786786 <kessahmed786@gmail.com>
2025-07-18 15:42:13 +0100

    feat: Completed Exercises

---
+++ chapter_2_variables_and_simple_data_types/numbers/ex2-10_favorite_number.py
@@ -0,0 +1,13 @@
+# Chapter 2 - Variables and Simple Data Types
+# ex2-10_favorite_number.py
+# Use a variable to represent your favorite number. Then, using that variable, create a message that reveals your favorite number. Print that message.
+# Kess Ahmed, 18th July 2025
+
+# Declare and assign variable favorite_number
+favorite_number = 23
+
+# Create message variable, that holds message
+message = f"My favorite number is: {favorite_number}"
+
+# Print message
+print(message)
---
+++ chapter_2_variables_and_simple_data_types/numbers/ex2-9_number_eight.py
@@ -0,0 +1,16 @@
+# Chapter 2 - Variables and Simple Data Types
+# ex2-9_number_eight.py
+# Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print() calls to see the results. You should create four lines that look like this: print(5 + 3). Your output should be four lines, with the number 8 appearing once on each line.
+# Kess Ahmed, 18th July 2025
+
+# Addition 8
+print(5 + 3)
+
+# Subtraction 8
+print(10 - 2)
+
+# Multiplication 8
+print(4 * 2)
+
+# Divition 8
+print(16 / 2)
