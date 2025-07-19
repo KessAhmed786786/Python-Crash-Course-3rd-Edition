ecf4a16455a5af90b643312ca042b5b72f54aee6
KessAhmed786786 <kessahmed786@gmail.com>
2025-07-19 13:45:13 +0100

    feat: Completed Exercise 3-11

---
+++ chapter_3_introducing_lists/avoiding_index_errors_when_working_with_lists/ex3-11_intentional_error.py
@@ -0,0 +1,16 @@
+# Chapter 3 - Introducing Lists
+# ex3-11_intentional_error.py
+# If you haven’t received an index error in one of your programs yet, try to make one happen. Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program.
+# Kess Ahmed, 19th July 2025
+
+# Fixed list of 3
+error_list = ['1','2','3']
+
+# Print list
+print(error_list)
+
+# Acessing a out of bounds error
+print(error_list[3])
+
+# Error
+# IndexError: list index out of range
