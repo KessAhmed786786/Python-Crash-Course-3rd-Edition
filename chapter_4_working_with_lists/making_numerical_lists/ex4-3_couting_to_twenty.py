b3460f9fcdc8e249d41f66e8dbccc60cdd7d5e3b
KessAhmed786786 <kessahmed786@gmail.com>
2025-07-20 15:20:17 +0100

    feat: Completed Exercise 4-3

---
+++ chapter_4_working_with_lists/making_numerical_lists/ex4-3_couting_to_twenty.py
@@ -0,0 +1,7 @@
+# Chapter 4 - Working with Lists
+# Use a for loop to print the numbers from 1 to 20,inclusive.
+# Kess Ahmed, 20th July 2025
+
+# Generate a list of numbers 1-20, using range()
+for value in range(1,21):
+    print(value)
