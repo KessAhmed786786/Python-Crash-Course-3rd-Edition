4db1046ba04d0d594f9030f36bfa8d0d06667c3c
KessAhmed786786 <kessahmed786@gmail.com>
2025-07-20 15:20:31 +0100

    feat: Completed Exercise 4-6

---
+++ chapter_4_working_with_lists/making_numerical_lists/ex4-6_odd_numbers.py
@@ -0,0 +1,14 @@
+# Chapter 4 - Working with Lists
+# Make a list of odd numbers, from 1-20, use a for loop to print each element
+# Kess Ahmed, 20th July 2025
+
+# Create a list, odd_numbers using list(range())
+odd_numbers = list(range(1,21,2))
+
+# Iterate using for loop in odd_numbers
+for odd_number in odd_numbers:
+    print(odd_number)
+
+# Other method
+for value in range (1,20,2):
+    print(value)
