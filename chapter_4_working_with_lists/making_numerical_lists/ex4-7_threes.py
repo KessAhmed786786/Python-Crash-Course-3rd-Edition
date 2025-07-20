fed657cfb6210f7d1fb0aa8e594843a54e4ef12f
KessAhmed786786 <kessahmed786@gmail.com>
2025-07-20 15:20:39 +0100

    feat: Completed Exercise 4-7

---
+++ chapter_4_working_with_lists/making_numerical_lists/ex4-7_threes.py
@@ -0,0 +1,8 @@
+# Chapter 4 - Working with Lists
+# Make a list of 3's from 3 to 30, for loop to print
+# Kess Ahmed, 20th July 2025
+
+# Use range, 3 arguments to generate a for loop 
+for value in range(3, 31, 3):
+    # Print value
+    print(value)
