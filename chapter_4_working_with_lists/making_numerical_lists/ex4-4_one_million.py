f80a6d7b81f398f1bb085c48b81f528b21172068
KessAhmed786786 <kessahmed786@gmail.com>
2025-07-20 15:20:23 +0100

    feat: Completed Exercise 4-4

---
+++ chapter_4_working_with_lists/making_numerical_lists/ex4-4_one_million.py
@@ -0,0 +1,12 @@
+# Chapter 4 - Working with Lists
+# Make a list of the numbers from one to one million, and then use a for loop to print the numbers. (If the output is taking too long, stop it by pressing CTRL-C or by closing the output window.)
+# Kess Ahmed, 20th July 2025
+
+# Generate a list from 1 to 1,000,000
+million = []
+million = list(range(1, 1_000_000))
+
+# Print each element using a for loop
+thousand = list(range(1, 1_000))
+for number in thousand:
+    print(number)
