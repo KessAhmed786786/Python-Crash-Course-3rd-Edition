76131bb8abf6b7120be9063a473519059d95aa5c
KessAhmed786786 <kessahmed786@gmail.com>
2025-07-20 15:20:54 +0100

    feat: Completed Exercise 4-9

---
+++ chapter_4_working_with_lists/making_numerical_lists/ex4-9_cube_comprehension.py
@@ -0,0 +1,9 @@
+# Chapter 4 - Working with Lists
+# Use list comprehnsion to generate a list of the first 10 cubes
+# Kess Ahmed, 20th July 2025
+
+# List [], value expression, for value in range(range)
+cubes = [value**3 for value in range(1,11)]
+
+# Print proof
+print(cubes)
