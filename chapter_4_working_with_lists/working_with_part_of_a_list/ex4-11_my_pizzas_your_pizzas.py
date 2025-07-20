f1c1b33a4d587bf7a369e5a226c1e764b287967c
KessAhmed786786 <kessahmed786@gmail.com>
2025-07-20 15:21:05 +0100

    feat: Completed Exercise 4-12

---
+++ chapter_4_working_with_lists/working_with_part_of_a_list/ex4-11_my_pizzas_your_pizzas.py
@@ -0,0 +1,25 @@
+# Chapter 4 - Working with Lists
+# Copy list of pizzas, called friend_pizzas. Add new to the original, and different one to friend_pizzas, the prove using for loop
+# Kess Ahmed, 20th July 2025
+
+# Copied list from before
+pizzas = ['cheese', 'chicken', 'hawaiian']
+
+# Copy list, using slice
+friend_pizzas = pizzas[:]
+
+# Append to pizzas
+pizzas.append('beef')
+
+# Append to friend_pizzas
+friend_pizzas.append('anchovy')
+
+# for loop iteration pizzas
+print("\nMy favourite pizzas are:")
+for pizza in pizzas:
+    print(pizza)
+
+# for loop iteration friend_pizzas
+print("\nMy friends favourite pizzas are:")
+for pizza in friend_pizzas:
+    print(pizza)
