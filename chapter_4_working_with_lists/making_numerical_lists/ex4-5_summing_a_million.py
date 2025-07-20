c3136cda5d80753c2ab55802a665f4f2fb8f7b18
KessAhmed786786 <kessahmed786@gmail.com>
2025-07-20 15:20:27 +0100

    feat: Completed Exercise 4-5

---
+++ chapter_4_working_with_lists/making_numerical_lists/ex4-5_summing_a_million.py
@@ -0,0 +1,15 @@
+# Chapter 4 - Working with Lists
+# 
+# Kess Ahmed, 20th July 2025
+
+# Generate a list of called million, from range 1, 1000000
+million = list(range(1,1_000_001))
+
+# Find min
+print(min(million))
+
+# Find max
+print(max(million))
+
+# Find sum
+print(sum(million))
