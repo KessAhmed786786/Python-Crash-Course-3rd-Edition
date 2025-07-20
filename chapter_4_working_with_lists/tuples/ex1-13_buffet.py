fb84787ef7dfaa5073d529ac7245ef64c362ea01
KessAhmed786786 <kessahmed786@gmail.com>
2025-07-20 15:22:50 +0100

    feat: Completed Exercise 4-13

---
+++ chapter_4_working_with_lists/tuples/ex1-13_buffet.py
@@ -0,0 +1,18 @@
+# Chapter 4 - Working with Lists
+# Kess Ahmed, 20th July 2025
+
+# Store 5 foods in tuple, Use for loop and print, revise the menu with 2 new elements
+
+# Create tuple called menu, assign 5 immutable dishes
+menu = ('jacket potato', 'shrimp', 'sea bass', 'new york steak', 'mushroom salad')
+
+# For loop iterate print
+for dish in menu:
+    print(dish)
+
+# Reassign menu, change 2 menu items
+menu = ('roast chicken', 'falafel', 'sea bass', 'new york steak', 'mushroom salad')
+
+# For loop iterate print
+for dish in menu:
+    print(dish)
