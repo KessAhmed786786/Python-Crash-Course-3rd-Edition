8901754db86a7d473b8b373c08990d189e5e2b06
KessAhmed786786 <kessahmed786@gmail.com>
2025-07-21 16:45:23 +0100

    feat: Completed Exercise 5-7

---
+++ chapter_5_if_statements/if_statements/ex5-7_favorite_fruit.py
@@ -0,0 +1,18 @@
+# Chapter 5 - If Statements
+# Kess Ahmed, 21st July 2025
+# List favorite fruits, write independet if statements to check for certain fruits, cap with string statement
+
+# Create a list
+favorite_fruits = ['banana','apple','orange']
+
+# Five if statements, to check if in the list
+if 'banana' in favorite_fruits:
+    print("You must bananas!")
+if 'apple' in favorite_fruits:
+    print("You must like apples!")
+if 'orange' in favorite_fruits:
+    print("You must like oranges!")
+if 'blueberries' in favorite_fruits:
+    print("You must like blueberrie!")
+if 'mango' in favorite_fruits:
+    print("You must like mangoes!")
