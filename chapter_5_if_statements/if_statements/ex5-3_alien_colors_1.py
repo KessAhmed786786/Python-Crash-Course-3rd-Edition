ba897b59250d0f08a5f5a98258015eaae77270d6
KessAhmed786786 <kessahmed786@gmail.com>
2025-07-21 16:45:03 +0100

    feat: Completed Exercise 5-3

---
+++ chapter_5_if_statements/if_statements/ex5-3_alien_colors_1.py
@@ -0,0 +1,16 @@
+# Chapter 5 - If Statements
+# Kess Ahmed, 21st July 2025
+# Create a variable alien_color, if statement if green
+
+# Create variable
+alien_color = 'green'
+
+# If statement
+if alien_color == 'green':
+    print("You earned 5 points!")
+
+# If statement failed
+if alien_color == 'red':
+    print("You earned points!")
+
+
