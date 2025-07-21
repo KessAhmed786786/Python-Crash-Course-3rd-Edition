e82a75833e749b0923e8e0727a3d7ae9d39e3874
KessAhmed786786 <kessahmed786@gmail.com>
2025-07-21 16:45:12 +0100

    feat: Completed Exercise 5-5

---
+++ chapter_5_if_statements/if_statements/ex5-5_alien_colors_3.py
@@ -0,0 +1,15 @@
+# Chapter 5 - If Statements
+# Kess Ahmed, 21st July 2025
+# Convert if-else to if-elif-else, print results for all colors
+
+# Create variables, comment out to test
+#alien_color = 'green'
+alien_color = 'yellow'
+#alien_color = 'red'
+
+if alien_color == 'green':
+    print("You earned 5 points!")
+elif alien_color == 'yellow':
+    print("You earned 10 points!")
+elif alien_color == 'red':
+    print("You earned 15 points!")
