fdf48f1a89bf998a54f3bb93cd64a99da58dca61
KessAhmed786786 <kessahmed786@gmail.com>
2025-07-23 19:34:46 +0100

    feat: Completed Exercise 6-2

---
+++ chapter_6_dictionaries/working_with_dictionaries/ex6-2_favorite_numbers.py
@@ -0,0 +1,14 @@
+# Chapter 6 - Dictionaries
+# Kess Ahmed, 23nd July 2025
+# Store a poll, list of names and favorite numbers
+
+# Create a dictionary
+favorite_numbers = {'kess': '42', 'ttar': '2015', 'blunder': '142', 'tdweeb': '5', 'russ': '33',}
+
+# Print each key and value in same string
+print("The poll of favorite numbers:")
+print(f"Kess: {favorite_numbers['kess']}")
+print(f"TTar: {favorite_numbers['ttar']}")
+print(f"Blunder: {favorite_numbers['blunder']}")
+print(f"TDweeb: {favorite_numbers['tdweeb']}")
+print(f"Russ: {favorite_numbers['russ']}")
