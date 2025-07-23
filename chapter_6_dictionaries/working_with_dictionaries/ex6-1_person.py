181b35d6e34461a962b1f08dc6f7e4685cef5c5d
KessAhmed786786 <kessahmed786@gmail.com>
2025-07-23 19:34:40 +0100

    feat: Completed Exercise 6-1

---
+++ chapter_6_dictionaries/working_with_dictionaries/ex6-1_person.py
@@ -0,0 +1,12 @@
+# Chapter 6 - Dictionaries
+# Kess Ahmed, 23nd July 2025
+# Create a dictionary about someone, then print all the values associated with that person
+
+# Create a dictionary
+person = {'first_name':'Kess', 'last_name':'Ahmed', 'age':'23', 'city':'London',}
+
+# Print each statement
+print(person['first_name'])
+print(person['last_name'])
+print(person['age'])
+print(person['city'])
