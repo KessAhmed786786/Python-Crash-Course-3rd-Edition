3b3c8444ce1d8a5f5e9598b4643ca3cd68adbb76
KessAhmed786786 <kessahmed786@gmail.com>
2025-07-24 17:20:51 +0100

    feat: Completed Exercise 7-7

---
+++ chapter_7_user_input_and_while_loops/introducing_while_loops/ex7-7_infinity.py
@@ -0,0 +1,26 @@
+# Chapter 7 - User Input and while Loops
+# Kess Ahmed, 24th July 2025
+# Convert exercise 7-6 into infinity loops
+
+# Infinite comparison
+# does not work
+
+# Infinte flag
+counter = 1
+flag = True
+while flag:
+    print(counter)
+    counter += 1
+    
+# Infinite active 
+counter = 1
+active = 1
+while active < 5:
+    print(counter)
+    counter += 1
+
+# Infinte break
+counter = 1
+while True:
+    print(counter)
+    counter +=1
