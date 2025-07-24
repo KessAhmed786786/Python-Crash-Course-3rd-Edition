3394e265d8f14b8adb09374e61b552fca197a362
KessAhmed786786 <kessahmed786@gmail.com>
2025-07-24 17:36:55 +0100

    refactor:  Removing repetition

--- chapter_7_user_input_and_while_loops/introducing_while_loops/ex7-6_three_exits.py
+++ chapter_7_user_input_and_while_loops/introducing_while_loops/ex7-6_three_exits.py
@@ -2,8 +2,7 @@
 # Kess Ahmed, 24th July 2025
 # Change 7-4, use conditional test in the while statement to stop the loop, use an active variable to contorl how long the loop runs, use a break statement to exit th eloop when the user enters a 'quit' value.
 
-# Non, flag quit
-"""
+# Prompts for all
 prompt = ("\nEnter a pizza topping: ")
 prompt += ("\nEnter 'quit' to end the program. ")
 
@@ -18,16 +17,11 @@ while pizza_topping != 'quit':
         print(pizza_topping)
     
 print("\nProgram has ended")
-"""
+
 
 # Active variable to control to control loop size
-"""
-prompt = ("\nEnter a pizza topping: ")
-prompt += ("\nEnter 'quit' to end the program. ")
-
 # Active control variable ready
 active_control = 0
-
 while active_control < 5:
     # User infroming of uses left
     print(f"\nYou have {5 - active_control} inputs left...")
@@ -38,13 +32,9 @@ while active_control < 5:
     active_control += 1
     
 print("\nProgram has ended")
-"""
+
 
 # Use break statement
-"""
-prompt = ("\nEnter a pizza topping: ")
-prompt += ("\nEnter 'quit' to end the program. ")
-
 while True:
     pizza_topping = input(prompt)
     if pizza_topping == 'quit':
@@ -52,16 +42,11 @@ while True:
     else:
         print(pizza_topping)
 print("\nProgram has ended")
-"""
+
 
 
 # Original basic version
-"""
-prompt = ("\nEnter a pizza topping: ")
-prompt += ("\nEnter 'quit' to end the program. ")
-
 active = True
-
 while active:
     pizza_topping = input(prompt)
     if pizza_topping == 'quit':
@@ -69,4 +54,4 @@ while active:
     else:
         print(pizza_topping)
 print("\nProgram has ended")
-"""
+
