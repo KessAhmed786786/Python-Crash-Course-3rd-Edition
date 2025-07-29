24c979b814d3e06ff7e19845f309b9539edf297b
KessAhmed786786 <kessahmed786@gmail.com>
2025-07-29 19:25:37 +0100

    feat: Completed Exercise 8-1

---
+++ chapter_8_functions/1_defining_a_function/Exercise 8-1: Message.py
@@ -0,0 +1,11 @@
+# Chapter 8 - Functions
+# Kess Ahmed, 29th July 2025
+# Write a function called display_message(), prints one message, call the function to make sure it works.
+
+# Create function called display_message()
+def display_message():
+    """Prints simple static message, of what I am learning"""
+    print("Today I am learning Chapter 8 - Functions")
+
+# Call function
+display_message()
