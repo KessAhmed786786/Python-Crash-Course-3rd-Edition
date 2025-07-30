7cba347e2b4064fdde382530ef99bea14351d805
KessAhmed786786 <kessahmed786@gmail.com>
2025-07-30 16:10:49 +0100

    refactor: Testing formatting

--- chapter_8_functions/4_passing_a_list/Exercise 8-9: Messages.py
+++ chapter_8_functions/4_passing_a_list/Exercise 8-9: Messages.py
@@ -2,6 +2,7 @@
 # Kess Ahmed, 29th July 2025
 # Pass a list of messages, that contain series of message strings, and then call the function
 
+
 # Create a function
 def generate_messages(message):
     """Auto populate a list, 20 times, then return"""
@@ -10,14 +11,17 @@ def generate_messages(message):
         auto_list.append(f"{message}: {value}")
     return auto_list
 
+
 # Genereate a list of messages
-messages = generate_messages('Hey this is a message')
+messages = generate_messages("Hey this is a message")
+
 
 # Create a function
 def show_messages(messages):
     """Show each message in list on each line"""
     for message in messages:
         print(message)
+
 
 # Use the list and print messages
 show_messages(messages)
